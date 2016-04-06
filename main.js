165dcd1a70154a1fec63523bcc893fd052edacea
Justin Van Voorst <justin.vanvoorst@gmail.com>
2016-04-06 16:03:24 -0600

    updated comments

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
+
+// Series of test cases 
+
 var test1 = [1,2,3,2,3,4,5]; // should be 4
 var test2 = [2,1,2,3,4]; // should be 3
 var test3 = [5,4,3,2,1,2,3];  //should be 2
 var test4 = [3,4,3,2,1]; // should be 1
 
-// function maxProfit(arr) {
-
-// 	var min = arr[0];
-// 	var max = 0;
-
-// 	for (i = 1; i < arr.length; i++) {
-// 		if (arr[i] < min) {
-// 			min = arr[i];
-// 		}
-// 		else if (arr[i] > max) {
-// 			max = arr[i];
-// 		}
-// 	}
-
-// 	console.log('min ' + min);
-// 	console.log('max ' + max);
-// }
-
+// basically a brute force method, first for loop iterates over each array element, inner for loop iterates over each
+// array element from i on since we cannot sell before buying. bestProfit is updated when a new maximum profit is found
 
 function maxProfit(arr) {
+
 	var bestProfit = 0;
+	
 	for (i = 0; i < arr.length; i++) {
+			
 		for (j = i + 1; j < arr.length; j++) {
+		
 			if (arr[j] > arr[i] &&  (arr[j] - arr[i]) > bestProfit) {
 				bestProfit = arr[j] - arr[i];
 			}
 		}
 	}
+
 	console.log(bestProfit);
 }
 
